1cb6b61997029c49cef9b77870c8bb44056693eb
Koban Davis <koban.davis@mondago.com>
2023-09-19 10:08:21 +0100

    support https

--- src/io.ts
+++ src/io.ts
@@ -1,11 +1,29 @@
 import http from 'http'
+import https from 'https'
+
 import { Server } from 'socket.io'
 import { Events } from './types'
+import fs from 'fs'
 
-const port = process.env.PORT || 5000
+const port = Number(process.env.PORT) || 5000
 
-const server = http.createServer()
-server.listen(port, () => console.log('Server started on port ' + port))
+let options: https.ServerOptions = {}
+let protocol: typeof http | typeof https
+
+if (fs.existsSync('./client-key.pem') && fs.existsSync('./client-cert.pem')) {
+	options.key = fs.readFileSync('./client-key.pem')
+	options.cert = fs.readFileSync('./client-cert.pem')
+	protocol = https
+} else {
+	protocol = http
+}
+
+const server = protocol
+	.createServer(options, (_, res) => {
+		res.writeHead(200)
+		res.end('hello world\n')
+	})
+	.listen(port, () => console.log('Server started on port ' + port))
 
 const io = new Server<Events.ClientToServer, Events.ServerToClient>(server, { cors: { origin: '*' } })
 
